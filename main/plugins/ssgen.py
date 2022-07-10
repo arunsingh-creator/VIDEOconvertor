4e61ff555cdefa053d4e83cee62b017f4fa40706
ARUN NOOB <83957038+arunsingh-creator@users.noreply.github.com>
2022-07-10 20:46:39 +0530

    Update ssgen.py

--- main/plugins/ssgen.py
+++ main/plugins/ssgen.py
@@ -11,6 +11,20 @@
 #  General Public License for more details.
 #
 #  License can be found in < https://github.com/vasusen-code/VIDEOconvertor/blob/public/LICENSE> .
+#    This file is part of the Compressor distribution.
+#    Copyright (c) 2021 Danish_00
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3.
+#
+#    This program is distributed in the hope that it will be useful, but
+#    WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#    General Public License for more details.
+#
+#    License can be found in 
+
 
 import os, time, subprocess, asyncio
 
@@ -82,3 +96,16 @@ async def screenshot(event, msg):
     for pic in pictures:
         os.remove(pic)
     os.remove(name)
+#    This file is part of the Compressor distribution.
+#    Copyright (c) 2021 Danish_00
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3.
+#
+#    This program is distributed in the hope that it will be useful, but
+#    WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#    General Public License for more details.
+#
+#    License can be found in 
