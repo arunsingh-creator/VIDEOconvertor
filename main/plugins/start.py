9be7514421d06e6f7eff096c6b47582eb0e6a436
ARUN NOOB <83957038+arunsingh-creator@users.noreply.github.com>
2022-07-10 20:47:09 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -11,6 +11,21 @@
 #  General Public License for more details.
 #
 #  License can be found in < https://github.com/vasusen-code/VIDEOconvertor/blob/public/LICENSE> .
+#    This file is part of the Compressor distribution.
+#    Copyright (c) 2021 Danish_00
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3.
+#
+#    This program is distributed in the hope that it will be useful, but
+#    WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#    General Public License for more details.
+#
+#    License can be found in 
+
+
 
 import os
 from .. import Drone
@@ -99,3 +114,16 @@ async def remt(event):
     except Exception:
         await event.edit("No thumbnail saved.")
     
+#    This file is part of the Compressor distribution.
+#    Copyright (c) 2021 Danish_00
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3.
+#
+#    This program is distributed in the hope that it will be useful, but
+#    WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#    General Public License for more details.
+#
+#    License can be found in 
